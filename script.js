54369c5c97a258b0bf3683dc9b2eeb94e96ca36c
Josh Zoellner <Josh.Zoellner@gmail.com>
2020-05-17 17:47:56 -0400

    added validations

--- script.js
+++ script.js
@@ -11,3 +11,29 @@
 </ol>
 <img src="${}">
 */
+
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoMass = document.querySelector("input[name=cargoMass]");
+      if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+            alert("All fields are required!");
+            event.preventDefault();
+      }
+      if (!isNaN(pilotName.value) || !isNaN(copilotName.value)) {
+            alert("Please make sure Pilot and Co-pilot's name are valid inputs!");
+            event.preventDefault();
+      }
+      if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
+            alert("Please make sure Fuel Level and Cargo Mass are valid inputs!");
+            event.preventDefault();
+      } 
+   });
+});
+
+
+
+
