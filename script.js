ea0f335c759d812ed98f9779b5c8a636acf1edc1
Josh Zoellner <Josh.Zoellner@gmail.com>
2020-05-17 19:50:26 -0400

    added more validations for checklist

--- script.js
+++ script.js
@@ -12,28 +12,59 @@
 <img src="${}">
 */
 
+
+
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
-   form.addEventListener("submit", function(event) {
-      let pilotName = document.querySelector("input[name=pilotName]");
-      let copilotName = document.querySelector("input[name=copilotName]");
-      let fuelLevel = document.querySelector("input[name=fuelLevel]");
-      let cargoMass = document.querySelector("input[name=cargoMass]");
-      if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
-            alert("All fields are required!");
-            event.preventDefault();
-      }
-      if (!isNaN(pilotName.value) || !isNaN(copilotName.value)) {
-            alert("Please make sure Pilot and Co-pilot's name are valid inputs!");
-            event.preventDefault();
-      }
-      if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
-            alert("Please make sure Fuel Level and Cargo Mass are valid inputs!");
-            event.preventDefault();
-      } 
-   });
+   let launchStatus = document.getElementById("launchStatus");
+   let faultyItems = document.getElementById("faultyItems");
+   let pilotStatus = document.getElementById("pilotStatus");
+   let copilotStatus = document.getElementById("copilotStatus");
+   let fuelStatus = document.getElementById("fuelStatus");
+   let cargoStatus = document.getElementById("cargoStatus");
+      form.addEventListener("submit", function(event) {
+         let pilotName = document.querySelector("input[name=pilotName]");
+         let copilotName = document.querySelector("input[name=copilotName]");
+         let fuelLevel = document.querySelector("input[name=fuelLevel]");
+         let cargoMass = document.querySelector("input[name=cargoMass]");
+         // Validations //
+         if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+               alert("All fields are required!");
+               event.preventDefault();
+         } else if (!isNaN(pilotName.value) || !isNaN(copilotName.value)) {
+               alert("Please make sure Pilot and Co-pilot's name are valid inputs!");
+               event.preventDefault();
+         } else if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
+               alert("Please make sure Fuel Level and Cargo Mass are valid inputs!");
+               event.preventDefault();
+         }  
+
+         // ^^^ End Validations ^^^ //
+
+         if (isNaN(pilotName.value) && isNaN(copilotName.value) && !isNaN(fuelLevel.value) && !isNaN(cargoMass.value)) {
+            pilotStatus.innerHTML = `Pilot ${pilotName.value} Ready`;
+            copilotStatus.innerHTML = `Pilot ${copilotName.value} Ready`;
+            if (fuelLevel.value < 10000){
+               fuelStatus.innerHTML = `There is not enough fuel for the journey.`;
+               faultyItems.style.visibility = "visible";
+               launchStatus.innerHTML = `Shuttle not ready for launch.`;
+               launchStatus.style.color = "red";
+               event.preventDefault();
+            } 
+            if (cargoMass.value > 10000){
+               cargoStatus.innerHTML = `There is too much mass for the shuttle to take off.`;
+               faultyItems.style.visibility = "visible";
+               launchStatus.innerHTML = `Shuttle not ready for launch.`;
+               launchStatus.style.color = "red";
+               event.preventDefault();
+            } 
+            if (fuelLevel.value > 9999 && cargoMass.value < 9999){               
+               launchStatus.innerHTML = `Shuttle is ready for launch.`;
+               launchStatus.style.color = "green";
+               faultyItems.style.visibility = "hidden";
+               event.preventDefault();
+            }
+         }
+      });
 });
-
-
-
 
